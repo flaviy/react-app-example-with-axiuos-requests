032b3a37eede41e73076dd64f5202b5d9778f831
Oleksandr Chebernin <chebernin@gmail.com>
2019-03-14 22:26:02 +0100

    define links

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './Blog.css';
 import Posts from './Posts/Posts';
+import NewPost from './NewPost/NewPost';
+import {Route, Link} from 'react-router-dom'
 
 class Blog extends Component {
   render () {
@@ -9,12 +11,21 @@ class Blog extends Component {
               <header>
                 <nav>
                   <ul>
-                    <li><a href="/">Home</a></li>
-                    <li><a href="/new-post">New Post</a></li>
+                    <li><Link to="/">Home</Link></li>
+                    <li>
+                      <Link to={{
+                        pathname: 'new-post',
+                        hash: 'somehash',
+                        search: 'search-phrase',
+                      }}>New Post</Link>
+                    </li>
                   </ul>
                 </nav>
               </header>
-              <Posts/>
+              <Route path="/" exact render={() => <h1>home</h1>}/>
+              <Route path="/" exact component={Posts}/>
+              <Route path="/new-post" component={NewPost}/>
+
             </div>
         );
     }
