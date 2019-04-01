411b511affcd29965395dca748f3f0bbd124dd34
Oleksandr Chebernin <chebernin@gmail.com>
2019-04-01 22:10:16 +0200

    Use <Switch> component How to make router match only first found routing and not render all matched

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './Blog.css';
 import Posts from './Posts/Posts';
 import NewPost from './NewPost/NewPost';
-import {Route, NavLink} from 'react-router-dom'
+import {Route, NavLink, Switch} from 'react-router-dom'
 import FullPost from './FullPost/FullPost'
 
 class Blog extends Component {
@@ -24,10 +24,11 @@ class Blog extends Component {
                 </nav>
               </header>
               <Route path="/" exact render={() => <h1>home</h1>}/>
-              <Route path="/" exact component={Posts}/>
-              <Route path="/new-post"  component={NewPost}/>
-              <Route path="/:postId" component={FullPost}/>
-
+              <Switch>
+                <Route path="/" exact component={Posts}/>
+                <Route path="/new-post"  component={NewPost}/>
+                <Route path="/:postId" component={FullPost}/>
+              </Switch>
             </div>
         );
     }
