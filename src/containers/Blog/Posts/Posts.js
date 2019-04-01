16684471c37f7466a0f961d056f37deea6d157ae
Oleksandr Chebernin <chebernin@gmail.com>
2019-04-02 00:07:16 +0200

    how to navigate programmatically

--- src/containers/Blog/Posts/Posts.js
+++ src/containers/Blog/Posts/Posts.js
@@ -11,9 +11,10 @@ class Posts extends Component {
     selectedPostId: null,
   }
   postClickedHandler = (postId) => {
-    this.setState({
+    this.props.history.push({pathname:'/'+postId});
+  /*  this.setState({
       selectedPostId: postId,
-    })
+    })*/
   }
 
   componentDidMount () {
@@ -37,13 +38,20 @@ class Posts extends Component {
   render () {
     let posts = this.state.posts.map(post => {
       return (
-        <Link to={'/'+post.id} key={post.id}>
+        <div>
           <Post
-            title={post.title}
+            title={'programatic ' + post.title}
             author={post.author}
             clicked={() => this.postClickedHandler(post.id)}
           />
-      </Link>
+          <Link to={'/'+post.id} key={post.id}>
+            <Post
+              title={post.title}
+              author={post.author}
+              clicked={() => this.postClickedHandler(post.id)}
+            />
+        </Link>
+        </div>
       )
     })
     return (
