f8ccb73381b38d3a9403849b3b942362ecf4545e
Oleksandr Chebernin <chebernin@gmail.com>
2019-04-11 22:47:35 +0200

    Redirect compoment

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './Blog.css';
 import Posts from './Posts/Posts';
 import NewPost from './NewPost/NewPost';
-import {Route, NavLink, Switch} from 'react-router-dom'
+import {Route, NavLink, Switch, Redirect} from 'react-router-dom'
 
 class Blog extends Component {
   render () {
@@ -25,6 +25,7 @@ class Blog extends Component {
         <Route path="/" exact render={() => <h1>home</h1>}/>
         <Switch>
           <Route path="/new-post" component={NewPost}/>
+          <Redirect from="/" to="/posts" />
           <Route path="/" component={Posts}/>
         </Switch>
       </div>
