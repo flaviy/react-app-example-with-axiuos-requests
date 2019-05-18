a461ddb8ae2b018655c349f14ed6afda5d4ec360
Oleksandr Chebernin <chebernin@gmail.com>
2019-05-18 19:27:27 +0200

    Lazy Loading with React.lazy and React <Suspense>

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,11 +1,13 @@
-import React, { Component } from 'react';
+import React, { Component, Suspense } from 'react';
 import './Blog.css';
-import Posts from './Posts/Posts';
+//import Posts from './Posts/Posts';
 /*
 import NewPost from './NewPost/NewPost';
 */
 import AsyncComponent from '../../hoc/asyncComponent';
 import {Route, NavLink, Switch, Redirect} from 'react-router-dom'
+
+const Posts = React.lazy(() => import('./Posts/Posts'));
 
 const AsyncNewPost = AsyncComponent(() => {
   return import('./NewPost/NewPost');
@@ -26,13 +28,21 @@ class Blog extends Component {
                   search: 'search-phrase=test',
                 }}>New Post</NavLink>
               </li>
+              <li>
+                <NavLink to={{
+                  pathname: '/posts',
+                  hash: 'somehash',
+                  search: 'search-phrase=test',
+                }}>Posts</NavLink>
+              </li>
             </ul>
           </nav>
         </header>
         <Switch>
           <Route path="/new-post" component={AsyncNewPost}/>
+          <Route path="/" exact render={() => <div>Home</div>}/>
           <Redirect from="/test" to="/posts" />
-          <Route path="/" exact component={Posts}/>
+          <Route path="/posts" exact render={()=> <Suspense fallback={<div>Loading...</div>}><Posts /></Suspense>}/>
           <Route  render={() => <h1>404 Page Not Found</h1>}/>
         </Switch>
       </div>
